94ae0a8c5d2f61fc3ad1f67c054d422c5d9a6eea
alvaromr <alvaro.martin.rodriguez@alumnos.upm.es>
2016-04-30 12:14:56 +0200

    First Test

---
+++ src/primefactors/PrimeFactors.java
@@ -0,0 +1,10 @@
+package primefactors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactors {
+    public static List<Integer> factorize(int number) {
+        return new ArrayList<>();
+    }
+}
---
+++ src/test/PrimeFactorsTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.Test;
+import primefactors.PrimeFactors;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrimeFactorsTest {
+    @Test
+    public void oneShouldNotHavePrimeFactors(){
+        assertEquals(Collections.emptyList(), PrimeFactors.factorize(1));
+    }
+}
